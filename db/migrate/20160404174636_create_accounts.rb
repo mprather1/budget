5e2e20ae4ab12ee6271153cedec749877a94fd2c
Michael Prather <mprather1@gmail.com>
2016-04-04 22:31:04 -0500

    account controller and models not tested

--- db/migrate/20160404174636_create_accounts.rb
+++ db/migrate/20160404174636_create_accounts.rb
@@ -1,7 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-
+      t.string :account_number
+      t.integer :balance
+      
       t.timestamps null: false
     end
   end
