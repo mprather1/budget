b863c39320782c70da1721338e6182dcb3f5630e
Michael Prather <mprather1@gmail.com>
2016-04-04 22:23:45 -0500

    Accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,16 +1,59 @@
 class AccountsController < ApplicationController
+
+
+  #before_action :correct_account, only: [:edit, :update, :show]
+
+
   def index
+    @accounts = Account.all
   end
 
   def show
+    @account = Account.find(params[:id])
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to account_path(@account)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update_attributes(account_params)
+      redirect_to account_path(@account)
+    else
+      render 'edit'
+    end
   end
 
   def delete
+    @account = Account.find(params[:id])
   end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to accounts_path
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:account_number, :balance)
+  end
+
 end
