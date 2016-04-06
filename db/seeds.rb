a66b781fd24f839a5569aea740c93a11a9988b36
Michael Prather <mprather1@gmail.com>
2016-04-05 21:55:29 -0500

    Account show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(first_name: "Nick", last_name: "Cage", username: "cagen", password: "1234")
 
-50.times do |x|
+5.times do |x|
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.user_name, password: Faker::Internet.password)
 end
 
-50.times do |x|
-  Account.create(account_number: "#{Faker::Number.number(10)}", balance: Faker::Number.between(1000, 10000), user_id: Faker::Number.between(1, 100))
+10.times do |x|
+  Account.create(account_number: "#{Faker::Number.number(10)}", balance: Faker::Number.between(1000, 10000), user_id: Faker::Number.between(1, 10))
 end
