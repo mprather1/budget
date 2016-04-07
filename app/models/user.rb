04ad055b7fb90cd8aa5c0b00fce025a6ef71dab5
Michael Prather <mprather1@gmail.com>
2016-04-06 21:05:39 -0500

    updated seed file, view, model for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 
   has_one :account
   before_create :build_default_account
-  #  validates_presence_of :password, :on => :create
+
+  #validates_presence_of :password, :on => :create
 #  validates_presence_of :password_confirmation
 #  validates_confirmation_of :password_confirmation
 
@@ -11,13 +12,11 @@ class User < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
-#  def create_account
-#    @account.create(account_number: "#{Faker::Number.number(10)}", balance: 0, user_id: current_user.id
-#  end
-private
+  private
 
   def build_default_account
     build_account
     true
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(first_name: "Nick", last_name: "Cage", username: "cagen", password: "1234")
 User.create(first_name: "Kill", last_name: "Bill", username: "killb", password: "1234")
 
 5.times do |x|
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.user_name, password: Faker::Internet.password)
 end
-
-#Account.create(account_number: "#{Faker::Number.number(10)}", balance: Faker::Number.between(1000, 10000), user_id: 1)
-#Account.create(account_number: "#{Faker::Number.number(10)}", balance: Faker::Number.between(1000, 10000), user_id: 2)
-
-#10.times do |x|
-#  Account.create(account_number: "#{Faker::Number.number(10)}", balance: Faker::Number.between(1000, 10000))
-#end
